d237c28d2c52f57c3998dff8c4515ff2116e6bec
AnasofficialGitHub <108149448+AnasofficialGitHub@users.noreply.github.com>
2022-10-02 23:00:23 +0530

    Update Library.java

--- Library.java
+++ Library.java
@@ -99,6 +99,7 @@ class Admin extends Login
 	String book[] = new String[50];
 	int id[] = new int[50];
 	int price[] = new int[50];
+	int stock[] = new int[50];
 
 	int n=0;
 	public void adminhome(){
@@ -106,24 +107,36 @@ class Admin extends Login
 		int i = scan.nextInt();
 		switch(i){
 		case 1:
-			view();
+			view();		
+			System.out.println("Press 0 to back");
+			int b= scan.nextInt();
+			if(b==0)
+				adminhome();
+			else 
+				System.out.println("Enter 0");
 			break;
 		
 		case 2:
 			add();
 			break;
+		case 3:
+			update();
+			break;
 		case 4:
 			delete();
 			break;
+		default:
+			System.out.println("Enter the correct number ");
+			adminhome();
 		}
 
 
 	}
 	public void view(){
-		System.out.println("S.no\t\b\bBook Id\tBook Name\tPrice");
+		System.out.println("S.no\t\b\bBook Id\t\tBook Name\t\tPrice\t\tStock");
 		for(int a=0;a<n;a++)
-			System.out.println((a+1)+".\t"+id[a]+"\t"+book[a]+"\t"+price[a]);
-		adminhome();
+			System.out.println((a+1)+".\t"+id[a]+"\t"+book[a]+"\t"+price[a]+"\t"+stock[a]);
+		
 		
 	}
 	public void add(){
@@ -139,29 +152,84 @@ class Admin extends Login
 			book[i]=scan1.nextLine();
 			System.out.println("Enter the book price ");
 			price[i]=scan.nextInt();
+			System.out.println("Enter the stock ");
+			stock[i]=scan.nextInt();
 			
 		}
 		n=n+num; 
 		adminhome();
 	}
 	public void delete(){
-		for(int a=0;a<n;a++)
-			System.out.println(book[a]);	
+		view();	
 		System.out.println("Press the number of the book to delete");
 		int del=scan.nextInt();
 		del=del-1;
 		String s = book[del];
-		for(int a=0;a<n;a++){
+		for(int a=0;a<n-1;a++){
 			if(book[a] == s){
 				String temp = book[a];
 				book[a]=book[a+1];
 				book[a+1]=temp;
+				swap(id[a],id[a+1]);
+				swap(price[a],price[a+1]);
+				swap(stock[a],stock[a+1]);
+				//int temp1 = id[a];
+				//id[a]=id[a+1];
+				//id[a+1]=temp1;
+				//temp1 = price[a];
+				//price[a]=price[a+1];
+				//price[a+1]=temp1;
+				//temp1 = stock[a];
+				//stock[a]=stock[a+1];
+				//stock[a+1]=temp1;
 			}
 		}
 		book[n-1]="";
 		n--;
 		adminhome();
 	}
+	public void swap(int x, int y){
+		int temp=x;
+		x=y;
+		y=temp;
+	}
+	public void update(){
+		view();
+		Scanner scan1 = new Scanner(System.in);
+		System.out.println("Enter the number of book you want to edit");
+		int edit = scan.nextInt();
+		edit = edit-1;
+		System.out.println("1.Edit ID\n2.Change Book Name\n3.Change stock\n4.Change Price\n5.Back");
+		int x=scan.nextInt();
+		switch(x){
+		case 1:
+			System.out.println("Enter the new id");
+			id[edit]=scan.nextInt();
+			break;
+		case 2:	
+			System.out.println("Enter the book name");
+			book[edit]=scan1.nextLine();
+			break;
+		case 3:
+			System.out.println("Enter the new stock");
+			stock[edit]=scan.nextInt();
+			break;
+		case 4:
+			System.out.println("Enter the new price");
+			price[edit]=scan.nextInt();
+			break;
+		case 5:
+			adminhome();
+			break;
+		default:
+			System.out.println("Enter the correct Number");
+			break;
+		}
+		update();
+			
+	}
+
+	
 }
 class User extends Login
 {
